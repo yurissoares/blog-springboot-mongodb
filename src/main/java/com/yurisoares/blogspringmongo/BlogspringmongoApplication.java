995f8cbf5069cdedcf6709e3d083ec3fc540fbd3
yurissoares <yurissoares@outlook.com>
2021-02-06 16:58:54 -0300

    Projeto criado

---
+++ src/main/java/com/yurisoares/blogspringmongo/BlogspringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.yurisoares.blogspringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogspringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogspringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yurisoares/blogspringmongo/BlogspringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurisoares.blogspringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogspringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
